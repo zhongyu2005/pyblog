29bd11732e1d60d83f2f9c0ac3cb358a69ff6e1b
zhongyu <zhongyu2005@gmail.com>
2019-08-01 18:36:28 +0800

    init yii2

---
+++ common/config/constant.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * backend => bkd.user_info
+ * common  => com.user_info
+ * console => cli.user_info
+ * api => api.user_info
+ * redis queue=>rds.queue_xxx_xxx
+ * mq queue => mq.e_xxx_xxx | mq.q_xxx_xxx
+ *
+ */
+
+
+define("BKD.USER_INFO", "bkd.user_info_");
---
+++ common/config/main.php
@@ -0,0 +1,44 @@
+<?php
+
+require __DIR__.'/constant.php';
+
+return [
+    /*
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    */
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'charset' => 'utf-8',
+    'components' => [
+        //file cache
+        'filecache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        //redis Cache
+//        'cache' => [
+//            'class' => 'common\components\Redis',
+//            'host' => '127.0.0.1',
+//            'port' => '6379',
+//            'database' => 0,
+//            'pconnect'=>1
+//        ],
+        //default db
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=pyblog',
+            'username' => 'pyblog',
+            'password' => '123456',
+            'charset' => 'utf8mb4',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
